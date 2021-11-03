1faa896d9051677c24eb62fbc935d0324d7b2fd0
wldldlsp <wldldlsp>
2021-11-03 21:09:34 +0900

    map 함수 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
+function Food({name,picture}){
+  return (
+    <div>
+    <h2>i like {name}</h2>
+    <img src={picture}/>
+    </div>
+    );
+}
+//props는 쉽게말해 매개변수
+
+
+const foodlike = [
+{
+  name : 'kimch',
+  image : 'https://img.wemep.co.kr/deal/7/717/4017177/ba09fc3cc86142f2ed6b55f23f983a7450669260.jpg',
+},
+{name: 'budae',
+  image : 'https://recipe1.ezmember.co.kr/cache/recipe/2018/04/24/002c46b34a8814b7d9848fc949eb9e1b1.jpg'
+}
+];
+
+
 function App() {
-  return <div>hello</div>;
+  return(
+    <div>
+      {foodlike.map(dish => (<Food name={dish.name} picture={dish.image} />
+      ))}     
+      </div>
+  ) ;
 }
 
 export default App;
