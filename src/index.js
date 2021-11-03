a21c0922f5b6ba62690b20ecb30eb3d8e9837ff7
wldldlsp <wldldlsp>
2021-11-03 19:27:08 +0900

    practice-git

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />,document.getElementById('root'));
+//'root'인 엘리먼트에다 App.js에 작성한 코드를 render 해준다.
+
